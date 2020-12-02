e6aafe02a49c868a68dcf4d3fcfe49a6ae2837f5
TedisAgolli <tedisagolli@gmail.com>
2020-12-01 23:37:05 -0500

    Bg color and change layout

--- components/ColorSquare.js
+++ components/ColorSquare.js
@@ -1,13 +1,15 @@
 import React from "react";
 
-export default function ColorSquare({ hex, isOriginal }) {
+export default function ColorSquare({ hex, isOriginal, tailwind }) {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col items-center m-4 ">
       <div className="text-lg">{isOriginal ? "Original" : "Tailwind"}</div>
       <div
-        className="w-24 h-24 m-2 bg-red-500"
+        className="w-28 h-28 m-2 rounded-lg outline-black"
         style={{ backgroundColor: hex }}
       ></div>
+      <div className="font-bold">{tailwind}</div>
+      <div className="text-gray-300">{hex}</div>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import { deltaE, rgb2lab } from "../utils/colors";
 import tailwindColors from "../utils/tailwindColors";
 import ColorSquare from "../components/ColorSquare";
 
+const isValidHex = (hex) => /^#([0-9A-F]{3}){1,2}$/i.test(hex);
 const hexToRgb = (hex) => {
   const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
   return result
@@ -18,6 +19,16 @@ const hexToLab = (hex) => {
   const rgb = hexToRgb(hex);
   return rgb2lab([rgb.r, rgb.g, rgb.b]);
 };
+
+const handle3DigitHex = (hexInput) =>
+  hexInput.length === 4
+    ? hexInput
+        // Remove leading #
+        .slice(1)
+        .split("")
+        .map((hex) => hex + hex)
+        .join("")
+    : hexInput;
 
 const tailwindColorsToLab = (colors) => {
   const allColors = [];
@@ -41,13 +52,17 @@ export default function Home() {
   const [closestCol, setClosestCol] = useState("");
   const onColorInputChange = (e) => {
     setColorInput(e.target.value);
+    findClosest(e.target.value);
   };
-  const findClosest = (e) => {
-    e.preventDefault();
-    const labColorInput = hexToLab(colorInput);
+  const findClosest = (hexInput) => {
+    if (!isValidHex(hexInput)) {
+      return;
+    }
+    const finalHex = handle3DigitHex(hexInput);
+    const labColorInput = hexToLab(finalHex);
     const comparedColors = labTailwind
       .map((col) => ({
-        deltaE: deltaE(col.lab, labColorInput),
+        deltaE: deltaE(col.lab, labColorInput).toFixed(2),
         ...col,
       }))
       .sort((a, b) => a.deltaE - b.deltaE);
@@ -55,30 +70,30 @@ export default function Home() {
   };
 
   return (
-    <div>
-      <div className="flex flex-col items-center bg-gray-200 min-h-screen">
-        <form onSubmit={findClosest}>
-          <input
-            type="text"
-            onChange={onColorInputChange}
-            value={colorInput}
-            class="shadow-md m-2 p-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-            placeholder="Enter hex color here"
-          />
-          <button
-            type="submit"
-            class="m-1 px-2.5 py-1.5 border border-transparent text-xs font-medium rounded shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            Find closest
-          </button>
-        </form>
-        {closestCol &&
-          `The closest color is ${closestCol.main}-${closestCol.sub} (hex ${closestCol.hex}) with a deltaE of ${closestCol.deltaE}`}
-        <div className="flex flex-row m-2">
-          <ColorSquare hex={colorInput} isOriginal />
-          <ColorSquare hex={closestCol.hex} isOriginal={false} />
+    <div className="flex flex-col items-center py-10 bg-gray-800 min-h-screen">
+      <h1 className="my-10 text-center text-white text-5xl">
+        Hex to Tailwind Converter
+      </h1>
+      <input
+        type="text"
+        onChange={onColorInputChange}
+        value={colorInput}
+        className="shadow-md m-2 p-2 border-gray-300 focus:outline-none focus:ring focus:border-blue-300 sm:text-sm rounded-md"
+        placeholder="Enter hex color here"
+      />
+      {isValidHex(colorInput) && closestCol && (
+        <div className="flex flex-col text-center text-white text-lg m-2">
+          {` Delta E = ${closestCol.deltaE}`}
+          <div className="flex flex-row justify-center m-2">
+            <ColorSquare hex={colorInput} isOriginal />
+            <ColorSquare
+              tailwind={`${closestCol.main}-${closestCol.sub}`}
+              hex={closestCol.hex}
+              isOriginal={false}
+            />
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
