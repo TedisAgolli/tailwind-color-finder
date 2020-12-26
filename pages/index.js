3dc824f80b990f33cee1a51c8f89b2dad22326d1
TedisAgolli <tedisagolli@gmail.com>
2020-12-26 12:58:08 -0500

    Change to use hex-to-tailwind from npm

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import { useState } from "react";
-import { deltaE, rgb2lab } from "../utils/colors";
-import tailwindColors from "../utils/tailwindColors";
 import ColorSquare from "../components/ColorSquare";
+import hexToTailwind from "hex-to-tailwind";
 import Head from "next/head";
 
 const explainDeltaE = (deltaE) => {
@@ -18,49 +17,7 @@ const explainDeltaE = (deltaE) => {
   }
 };
 const isValidHex = (hex) => /^#([0-9A-F]{3}){1,2}$/i.test(hex);
-const hexToRgb = (hex) => {
-  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-  return result
-    ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16),
-      }
-    : null;
-};
 
-const hexToLab = (hex) => {
-  const rgb = hexToRgb(hex);
-  return rgb2lab([rgb.r, rgb.g, rgb.b]);
-};
-
-const handle3DigitHex = (hexInput) =>
-  hexInput.length === 4
-    ? hexInput
-        // Remove leading #
-        .slice(1)
-        .split("")
-        .map((hex) => hex + hex)
-        .join("")
-    : hexInput;
-
-const tailwindColorsToLab = (colors) => {
-  const allColors = [];
-  Object.entries(colors).forEach((color) => {
-    const colorName = color[0];
-    Object.entries(color[1]).forEach((subColor) => {
-      allColors.push({
-        main: colorName,
-        sub: subColor[0],
-        hex: subColor[1],
-        lab: hexToLab(subColor[1]),
-      });
-    });
-  });
-  return allColors;
-};
-
-const labTailwind = tailwindColorsToLab(tailwindColors);
 export default function Home() {
   const [colorInput, setColorInput] = useState("");
   const [closestCol, setClosestCol] = useState("");
@@ -72,15 +29,7 @@ export default function Home() {
     if (!isValidHex(hexInput)) {
       return;
     }
-    const finalHex = handle3DigitHex(hexInput);
-    const labColorInput = hexToLab(finalHex);
-    const comparedColors = labTailwind
-      .map((col) => ({
-        deltaE: deltaE(col.lab, labColorInput).toFixed(2),
-        ...col,
-      }))
-      .sort((a, b) => a.deltaE - b.deltaE);
-    setClosestCol(comparedColors[0]);
+    setClosestCol(hexToTailwind(hexInput));
   };
 
   return (
@@ -114,8 +63,8 @@ export default function Home() {
           <div className="flex flex-row justify-center m-2">
             <ColorSquare hex={colorInput} isOriginal />
             <ColorSquare
-              tailwind={`${closestCol.main}-${closestCol.sub}`}
-              hex={closestCol.hex}
+              tailwind={closestCol.tailwind}
+              hex={closestCol.tailwindHex}
               isOriginal={false}
             />
           </div>
